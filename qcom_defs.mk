2af07c9146fa02b840b00487110508bd3e278154
Ctps123 <79877353+Ctps123@users.noreply.github.com>
2024-10-06 04:27:56 -0300

    Update qcom_defs.mk

--- qcom_defs.mk
+++ qcom_defs.mk
@@ -12,4 +12,4 @@ UM_4_19_BENGAL_FAMILY := bengal
 UM_5_4_FAMILY := lahaina holi
 UM_5_10_FAMILY := taro parrot
 UM_5_15_FAMILY := kalama
-UM_5_15_BENGAL_FAMILY := bengal #khaje_divar
+UM_5_15_BENGAL_FAMILY := bengal_515 #khaje_divar
